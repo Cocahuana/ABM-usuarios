ef2a5452c60b792e5b0ca71b69f1cb0d195dc647
Ezequiel <ezequieldominguez11@gmail.com>
2022-09-22 14:14:02 -0300

    feat: CustomInput component, structure create Job Posting

--- client/src/components/BtnGoBack.jsx
+++ client/src/components/BtnGoBack.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import {
+	Button,
+	useDisclosure,
+	useColorModeValue,
+	useColorMode,
+} from "@chakra-ui/react";
 function BtnGoBack() {
 	const navigate = useNavigate();
-
+	const { colorMode, toggleColorMode } = useColorMode();
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	return (
-		<Button onClick={() => navigate(-1)} className='btn btn-light'>
+		<Button
+			bg={useColorModeValue(
+				"var(--primary-light)",
+				"var(--primary-dark)"
+			)}
+			onClick={() => navigate(-1)}>
 			Go back
 		</Button>
 	);
--- client/src/components/Forms/CreateJobPost.jsx
+++ client/src/components/Forms/CreateJobPost.jsx
@@ -158,9 +158,128 @@ function CreateJobPost() {
 							errors={errors.nombre}
 							handleOnChange={(e) => handleOnChange(e)}
 						/>
-						
+						<CustomInput
+							title={"Positions to cover"}
+							name={"cantPuestos"}
+							type={"number"}
+							placeholder={"4"}
+							value={jobPost.cantPuestos}
+							errors={errors.cantPuestos}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Competencies"}
+							name={"competencias"}
+							type={"text"}
+							placeholder={"Active listener"}
+							value={jobPost.competencias}
+							errors={errors.competencias}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Duration"}
+							name={"duracion"}
+							type={"text"}
+							placeholder={"1 screening. 3 technical interviews"}
+							value={jobPost.duracion}
+							errors={errors.duracion}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Skill"}
+							name={"skill"}
+							type={"text"}
+							placeholder={"Javascript"}
+							value={jobPost.skill}
+							errors={errors.skill}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"City"}
+							name={"ciudad"}
+							type={"text"}
+							placeholder={"Miami"}
+							value={jobPost.ciudad}
+							errors={errors.ciudad}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
 					</Row>
 					<Row>
+						<CustomInput
+							title={"Contract type"}
+							name={"contratacionTipo"}
+							type={"text"}
+							placeholder={"Contractor"}
+							value={jobPost.contratacionTipo}
+							errors={errors.contratacionTipo}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Position Budget"}
+							name={"salario"}
+							type={"number"}
+							placeholder={"1500"}
+							value={jobPost.salario}
+							errors={errors.salario}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Job preference"}
+							name={"preferenciaJob"}
+							type={"text"}
+							placeholder={"Open to relocate"}
+							value={jobPost.preferenciaJob}
+							errors={errors.preferenciaJob}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"City preference"}
+							name={"preferenciaCiudad"}
+							type={"text"}
+							placeholder={"Boston"}
+							value={jobPost.preferenciaCiudad}
+							errors={errors.preferenciaCiudad}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Company preference"}
+							name={"preferenciaOrganismo"}
+							type={"text"}
+							placeholder={"Microsoft"}
+							value={jobPost.preferenciaOrganismo}
+							errors={errors.preferenciaOrganismo}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Residence"}
+							name={"residencia"}
+							type={"text"}
+							placeholder={"Uk, London"}
+							value={jobPost.residencia}
+							errors={errors.residencia}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+						<CustomInput
+							title={"Questions to the candidate"}
+							name={"preguntas"}
+							type={"text"}
+							placeholder={"How Would You Describe Yourself?"}
+							value={jobPost.preguntas}
+							errors={errors.preguntas}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+					</Row>
+					<Row>
+						<CustomInput
+							title={"Seniority"}
+							name={"jobLevel"}
+							type={"text"}
+							placeholder={"Junior Advanced"}
+							value={jobPost.jobLevel}
+							errors={errors.jobLevel}
+							handleOnChange={(e) => handleOnChange(e)}
+						/>
+
 						<Form.Group as={Col} className='mb-3'>
 							<Form.Label>Candidatos</Form.Label>
 							<Select
