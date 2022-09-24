87433965634727e2b1a78bbe300d850f19dec70c
Ezequiel <ezequieldominguez11@gmail.com>
2022-09-23 21:33:49 -0300

    setState

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {getPersonas} from "../../actions";
 import {useDispatch, useSelector} from "react-redux";
